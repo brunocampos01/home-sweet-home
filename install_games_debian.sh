810d89a39e287267ed91498f1738b27861127ff8
Bruno Campos <brunocampos01@gmail.com>
2023-04-15 18:56:19 -0300

    Update install_games_debian.sh

--- install_games_debian.sh
+++ install_games_debian.sh
@@ -26,3 +26,14 @@ flatpak install flathub net.lutris.Lutris
 wget http://repo.steampowered.com/steam/archive/precise/steam_latest.deb
 sudo dpkg -i steam_latest.deb
 sudo apt-get install -f
+
+
+# bottles
+flatpak install Bottles
+
+
+# ---------------------------------------------------------------
+# Install Libraries
+# ---------------------------------------------------------------
+# Wine
+wget https://github.com/bottlesdevs/wine/releases/download/caffe-7.20/caffe-7.20-x86_64.tar.xz
